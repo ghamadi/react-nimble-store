02ae934080ccd762ba8bf405dd1a737f4f566c3e
Ghaleb Hamadi <ghaleb.hamadi@gmail.com>
2023-04-05 04:32:40 +0300

    Configure update tsconfig & vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000
+  },
+
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, './src')
+      }
+    ]
+  },
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'ViteButton',
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    sourcemap: true,
+    emptyOutDir: true
+  },
+
+  plugins: [react(), dts()]
+});
