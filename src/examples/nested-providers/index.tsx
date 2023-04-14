2306a351cc39457be68eb11fd692034c8707f017
Ghaleb Hamadi <ghaleb.hamadi@gmail.com>
2023-04-14 16:20:08 +0300

    update example

--- src/examples/nested-providers/index.tsx
+++ src/examples/nested-providers/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback } from 'react';
+import { ChangeEvent, useCallback, useEffect, useRef } from 'react';
 import { createStore } from '~/lib/create-store';
 
 interface Store {
@@ -47,7 +47,10 @@ function SliderConfigProvider(props: { step: number }) {
     >
       <div style={{ border: '1px solid', padding: 20 }}>
         <SliderWrapper />
-        <DisplayedApples />
+        <DisplayedIcons />
+        <div style={{ margin: 10 }}>
+          <NonReactiveConsumer />
+        </div>
       </div>
     </SliderStore.Provider>
   );
@@ -59,12 +62,13 @@ function SliderWrapper() {
   return (
     <div>
       <h4 style={{ margin: '0px 0 10px 0' }}> Step: {step} </h4>
+      <hr />
       <Slider />
     </div>
   );
 }
 
-function DisplayedApples() {
+function DisplayedIcons() {
   const count = SliderStore.useStore((state) => state.count);
 
   return (
@@ -81,6 +85,8 @@ function Slider() {
   const count = SliderStore.useStore((state) => state.count);
   const setCount = SliderStore.useStore((state) => state.setCount);
 
+  const renderRef = useRef(0);
+
   const handleSliderChange = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
       const value = +event.target.value;
@@ -90,17 +96,49 @@ function Slider() {
   );
 
   return (
-    <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-      <input
-        type="range"
-        step={step}
-        min="0"
-        max="50"
-        value={count}
-        onChange={handleSliderChange}
-        style={{ flex: 1 }}
-      />
-      <span>{count}</span>
+    <>
+      <p>Renders: {++renderRef.current}</p>
+      <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+        <input
+          type="range"
+          step={step}
+          min="0"
+          max="50"
+          value={count}
+          onChange={handleSliderChange}
+          style={{ flex: 1 }}
+        />
+        <span>{count}</span>
+      </div>
+    </>
+  );
+}
+
+function NonReactiveConsumer() {
+  // When called, subscribes to changes in the store based on the provided selector & predicate
+  // If no selector is passed, any change in the store is detected
+  const subscribe = SliderStore.useSubscribe((state) => state.count);
+
+  // A function that returns the current state in the store
+  const getState = SliderStore.useGetState();
+
+  const renderRef = useRef(0);
+
+  useEffect(() => {
+    // Subscribe to changes and return the unsubscribe callback to clean up on unmount
+    return subscribe(() => console.log('NEW VALUE', getState().count));
+  }, [subscribe, getState]);
+
+  return (
+    <div style={{ border: '1px solid red', padding: 10 }}>
+      <p>Renders: {++renderRef.current}</p>
+      <p>This component does NOT re-render, even though it is listening to changes in the store.</p>
+      <hr style={{ width: '100%' }} />
+
+      <p>Change the slider and check the console to see DETECTED CHANGE</p>
+
+      <span>Click this button to log the current state in the store.</span>
+      <button onClick={() => console.log(getState())}>Log Current State</button>
     </div>
   );
 }
