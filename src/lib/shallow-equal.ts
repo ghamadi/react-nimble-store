04459bc638db220d26bb4f7671e73413eb7a7046
Ghaleb Hamadi <ghaleb.hamadi@gmail.com>
2023-04-26 21:59:00 +0300

    add doc comments

--- src/lib/shallow-equal.ts
+++ src/lib/shallow-equal.ts
@@ -1,5 +1,9 @@
 type ObjectType = Record<string | number | symbol, unknown>;
 
+/**
+ * Shallowly compares two arguments of unknown types.
+ * Performs runtime type checks for the comparison.
+ */
 export function shallowEqual(a: unknown, b: unknown) {
   if (typeof a !== 'object' && typeof b !== 'object') {
     return a === b;
@@ -23,6 +27,9 @@ export function shallowEqual(a: unknown, b: unknown) {
   return shallowEqualObject(a as ObjectType, b as ObjectType);
 }
 
+/**
+ * Shallowly compares two arrays. Values are compared with strict equality.
+ */
 export function shallowEqualArray(arr1: unknown[], arr2: unknown[]) {
   if (arr1 === arr2) {
     return true;
@@ -31,6 +38,9 @@ export function shallowEqualArray(arr1: unknown[], arr2: unknown[]) {
   return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
+/**
+ * Shallowly compares two objects. Values are compared with strict equality.
+ */
 export function shallowEqualObject(obj1: ObjectType, obj2: ObjectType) {
   const keys1 = Reflect.ownKeys(obj1);
   const keys2 = Reflect.ownKeys(obj2);
